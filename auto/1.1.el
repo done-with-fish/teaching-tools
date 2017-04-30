052de2d61c2974b9243e7d889198c9cc9c89ee70
Brian Fitzpatrick <bfitzpat@math.duke.edu>
2017-04-30 05:07:04 -0400

    Fixed subscripts in row reductions in 1.1.tex

---
+++ auto/1.1.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "1.1"
+ (lambda ()
+    (LaTeX-add-labels
+     "eq:motivation")
+    (TeX-run-style-hooks
+     "latex2e"
+     "art12"
+     "article"
+     "12pt"
+     "packages"
+     "style")))
+
